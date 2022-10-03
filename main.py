b34047577b6599b52ea7f8029267fb2721e82a74
Gengar-lab <beneficialzebra4707@gmail.com>
2022-10-03 15:45:22 +0530

    Update main.py
    Updated for 2022

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-from pandas.plotting import parallel_coordinates
-import pandas as pd
 import matplotlib.ticker as mtick
+import pandas as pd
+from pandas import DataFrame
+from pandas.plotting import parallel_coordinates
 
 
-def transform_data_year_only(data, column_name, year):
+def transform_data_year_only(data: DataFrame, column_name: str, year: int) -> DataFrame:
     data.columns = ['Technology', 'loved', 'wanted', 'dreaded']
     data.set_index('Technology', inplace=True)
     data = data[[column_name]]
@@ -12,7 +13,7 @@ def transform_data_year_only(data, column_name, year):
     return data
 
 
-def merge_rows(data, rowname_a, rowname_b):
+def merge_rows(data: DataFrame, rowname_a: str, rowname_b: str) -> DataFrame:
     # TODO: Do it properly
     data = data.T
     data[rowname_a] = data[rowname_a].fillna(data[rowname_b])
@@ -22,28 +23,16 @@ def merge_rows(data, rowname_a, rowname_b):
 
 
 def main():
-    data_2015 = pd.read_csv('2015_loved_dreaded.csv')
-    data_2015 = transform_data_year_only(data_2015, 'loved', 2015)
-    data_2016 = pd.read_csv('2016_loved_dreaded.csv')
-    data_2016 = transform_data_year_only(data_2016, 'loved', 2016)
-    data_2017 = pd.read_csv('2017_loved_dreaded.csv')
-    data_2017 = transform_data_year_only(data_2017, 'loved', 2017)
-    data_2018 = pd.read_csv('2018_loved_dreaded.csv')
-    data_2018 = transform_data_year_only(data_2018, 'loved', 2018)
-    data_2019 = pd.read_csv('2019_loved_dreaded.csv')
-    data_2019 = transform_data_year_only(data_2019, 'loved', 2019)
-    data_2020 = pd.read_csv('2020_loved_dreaded.csv')
-    data_2020 = transform_data_year_only(data_2020, 'loved', 2020)
-    data_2021 = pd.read_csv('2021_loved_dreaded.csv')
-    data_2021 = transform_data_year_only(data_2021, 'loved', 2021)
+    years = list(range(2015, 2023))
+    dataframes = dict.fromkeys(years)
+    for year in years:
+        dataframes[year] = pd.read_csv(f'csv_data/{year}_loved_dreaded.csv')
+        dataframes[year] = transform_data_year_only(dataframes[year], 'loved', year)
 
     # Join the dataframes
-    data = pd.merge(data_2015, data_2016, how='outer', left_index=True, right_index=True)
-    data = pd.merge(data, data_2017, how='outer', left_index=True, right_index=True)
-    data = pd.merge(data, data_2018, how='outer', left_index=True, right_index=True)
-    data = pd.merge(data, data_2019, how='outer', left_index=True, right_index=True)
-    data = pd.merge(data, data_2020, how='outer', left_index=True, right_index=True)
-    data = pd.merge(data, data_2021, how='outer', left_index=True, right_index=True)
+    data = dataframes[2015]
+    for year in years[1:]:
+        data = pd.merge(data, dataframes[year], how='outer', left_index=True, right_index=True)
 
     # Plot the data
     data = merge_rows(data, 'Bash/Shell', 'Bash/Shell/PowerShell')
@@ -51,12 +40,11 @@ def main():
     print(f'Data points: {len(data)}')
 
     data.reset_index(level=0, inplace=True)
-    data = data.sort_values(by=[str(i) for i in range(2021, 2015, -1)], ascending=False)
+    data = data.sort_values(by=[str(i) for i in years[::-1]], ascending=False)
     ax = parallel_coordinates(data, 'Technology', colormap=plt.get_cmap('gist_ncar'), linewidth=3, alpha=0.7, marker='o', markersize=7)
-
     ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
     plt.title('Most Loved Programming Languages '
-              '\nStack Overflow Developer Survey data 2011-2021', fontsize=22)
+              '\nStack Overflow Developer Survey data 2015-2022', fontsize=22)
 
     plt.show()
 
